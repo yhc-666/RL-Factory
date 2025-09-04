d2a8b2c8f158119627cdf54233a84fb80834f12e
yang <yang@MBP-K63DWF250X-0001.local>
2025-09-04 19:30:30 +0800

    configged rag

--- rag_server/launch.sh
+++ rag_server/launch.sh
@@ -6,10 +6,10 @@
 
 nvcc --version
 
-file_path=/your/path/to/data/rag_data
-index_file=$file_path/e5_Flat.index
-corpus_file=$file_path/wiki-18.jsonl
-retriever=/your/path/to/intfloat/e5-base-v2
+#file_path=/your/path/to/data/rag_data
+index_file=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtsearch-assistant/ai-search/yanghaocheng04/data/data/e5_Flat.index
+corpus_file=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtsearch-assistant/ai-search/yanghaocheng04/data/data/wiki-18.jsonl
+retriever=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtsearch-assistant/ai-search/yanghaocheng04/data/models/e5-base-v2
 
 python3 rag_server/retrieval_server.py --index_path $index_file \
                                             --corpus_path $corpus_file \
